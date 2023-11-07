2745d8c70ab01e4e4a6ad462f7222418a4444efe
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-11-07 14:08:41 -0700

    configure database

--- server/app.py
+++ server/app.py
@@ -1,4 +1,7 @@
 from config import app
 
+
+
+
 if (__name__ == "__main__"):
     app.run(port=8000, debug=True)
--- server/config.py
+++ server/config.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 load_dotenv()
 
 app = Flask(__name__) #create an instance of the Flask class called app (this is the app object)
 app.secret_key = os.getenv("FLASK-SECRET") #set the secret key for the app
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI") #set the database URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #set the track modifications to false - less noisy terminal
+
+db = SQLAlchemy(app) #create an instance of the SQLAlchemy class called db (this is the db object)
