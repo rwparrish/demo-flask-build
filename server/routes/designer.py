43bfc8bc5a5ae51ffcdcf0d9f7d2184159ed26c5
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-11-15 09:34:16 -0700

    working POST for designers

--- server/routes/designer.py
+++ server/routes/designer.py
@@ -1,5 +1,6 @@
+from flask import request 
 from flask_restful import Resource
-from config import api
+from config import api, db
 from models.models import Designer
 
 
@@ -7,5 +8,11 @@ class DesignerResource(Resource):
     def get(self):
         designers = [designer.to_dict() for designer in Designer.query.all()]
         return designers, 200
+    
+    def post(self):
+        designer = Designer(**request.get_json())
+        db.session.add(designer) #add designer to the session / transaction
+        db.session.commit() #add to the database
+        return designer.to_dict(), 201
     
 api.add_resource(DesignerResource, "/api/designers") #use /api here to separate backend routes from frontend routes because they will be on one domain
